91720a2570494faf521cb5426cae96c9c868ec42
Sathveegan <sathve@gmail.com>
2023-08-29 20:24:08 +0530

    Starting routes with files

--- Server/index.js
+++ Server/index.js
@@ -8,9 +8,45 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import { error } from "console";
 
 /* CONFIGURATION */ // all the midleware configurations (run between different things)
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
-const app = express();
+const app = express(); // invoke express application
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true })); // limit the size of the images
+app.use(cors()); // allow to connect to the frontend
+app.use("/assets", express.static(path.join(__dirname, "assets"))); // serve static files
+
+/* FILE STORAGE */ // github repo: multer
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/assets");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+/* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register);   // upload picture locally public/assets folder
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 6000;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
