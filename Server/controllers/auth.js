94845ff944a530b0f6af7c20f7a898eaa6d51ff6
Sathveegan <sathve@gmail.com>
2023-08-29 21:54:15 +0530

    created model with educonnect data

--- Server/controllers/auth.js
+++ Server/controllers/auth.js
@@ -1,3 +1,48 @@
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import User from '../models/User.js';
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+/* REGISTER USER */
+export const register = async (req, res) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      picturePath,
+      highestDegree,
+      university,
+      fieldOrMajor,
+      occupation,
+      industry,
+      experienceLevel,
+      friends,
+      location,
+    } = req.body;
+
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: passwordHash,
+      picturePath,
+      highestDegree,
+      university,
+      fieldOrMajor,
+      occupation,
+      industry,
+      experienceLevel,
+      friends,
+      location,
+      viewedProfile: Math.floor(Math.random() * 10000),
+      impressions: Math.floor(Math.random() * 10000),
+    });
+
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (error) {}
+};
